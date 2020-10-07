9420f45b392966cb853576ce24b41d1e8924f619
Ryan Renwick <RRenwick@live.com>
2020-10-06 20:48:13 -0500

    idk again

--- Components/Nav.js
+++ Components/Nav.js
@@ -2,9 +2,9 @@ export default () => `
 <nav class="active">
   <div class="topnav">
     <a href="index.html">Home</a>
-    <a href="about.html">About</a>
-    <a href="howTo.html">How To</a>
-    <a href="setUp.html">Set Up</a>
+    <a href="About.html">About</a>
+    <a href="HowTo.html">How To</a>
+    <a href="SetUp.html">Set Up</a>
   </div>
 </nav>
 `;
--- Components/Views/index.js
+++ Components/Views/index.js
@@ -1,4 +1,4 @@
 export { default as About } from "./About";
 export { default as HowTo } from "./HowTo";
-export { default as Setup } from "./Setup";
+export { default as SetUp } from "./SetUp";
 export { default as Home } from "./Home";
