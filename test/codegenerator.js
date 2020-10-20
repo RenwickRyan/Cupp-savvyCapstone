6fd0c983a3dd20253e1a8cab59ce696435826d2c
Ryan Renwick <RRenwick@live.com>
2020-10-20 15:41:37 -0500

    testing code & styles

---
+++ test/codegenerator.js
@@ -0,0 +1,50 @@
+const colors = [
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "brown",
+  "orange",
+  "purple",
+  "pink",
+  "black",
+  "teal"
+];
+var randColor = colors[Math.floor(Math.random() * colors.length)];
+
+console.log(randColor);
+
+var teams = [
+  "cardinals",
+  "braves",
+  "reds",
+  "pirates",
+  "red socks",
+  "angels",
+  "yankees",
+  "royals",
+  "cubs",
+  "dodgers"
+];
+var randTeam = teams[Math.floor(Math.random() * teams.length)];
+
+console.log(randTeam);
+
+const subjects = [
+  "math",
+  "reading",
+  "english",
+  "art",
+  "gym ",
+  "science",
+  "health",
+  "cooking",
+  "physics",
+  "geography"
+];
+var randSubject = subjects[Math.floor(Math.random() * subjects.length)];
+
+console.log(randSubject);
+
+// var codebutton = document.getElementsByClassName("btn");
+// var displayColor = document.getElementById("test");
