70d41822a009718311cc2af27392bbe6d11e6977
Ryan Renwick <RRenwick@live.com>
2020-11-09 21:54:11 -0600

    add step image fix

--- Components/Views/Step10.js
+++ Components/Views/Step10.js
@@ -2,8 +2,8 @@ export default st => `
 <div class="start" id="start">
 <h3 id="steps">Step 10: Allow the coffee to cool until the 12-15 minute mark, then begin tasting Get a small amount of the coffee in your spoon, then SLURP!<br><br> * be sure to rinse your spoon in hot water between cups *</h3>
           <div id="step-imgs">
-          <img class="steps" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/coffeeinspoon.jpeg?raw=true" style="width:300px;height:300px;">
-          <img class="steps" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/slurpsup.jpeg?raw=true" style="width:300px;height:300px;">
+          <img class="steps" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/coffeeinspoon.jpeg?raw=true" style="width:300px;height:400px;">
+          <img class="steps" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/slurpsup.jpeg?raw=true" style="width:300px;height:400px;">
         </div>
           <p id="setupdetail">Slurping the coffee allows it to spray across your entire palate, letting you taste a wider range of flavors <br><br> Take note of changes to the flavor of the coffee.<br>It will continue to change as the coffee cools <br><br> Allow for several passes over 15 minutes or so </p>
 </div>
--- Components/Views/Step8.js
+++ Components/Views/Step8.js
@@ -2,7 +2,7 @@ export default st => `
 <div class="start" id="start">
 <h3 id="steps">Step 8: At the 4 minute mark, break the crust</h3>
           <div id="step-imgs">
-          <img class="steps" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/breakingcrust.jpeg?raw=true" style="width:300px;height:300px;">
+          <img class="steps" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/breakingcrust.jpeg?raw=true" style="width:300px;height:400px;">
         </div>
           <p id="setupdetail">Get in close to the bowl, then using a spoon push the coffee around slowly front to back <br> <br> Be sure to rinse your spoon in hot water between each bowl</p>
 </div>
