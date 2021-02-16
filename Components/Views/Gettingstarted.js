b631f0abe4995cff33897a327c9081fed5acda84
Ryan Renwick <RRenwick@live.com>
2021-02-16 09:58:06 -0600

    fix images for small screens

--- Components/Views/Gettingstarted.js
+++ Components/Views/Gettingstarted.js
@@ -14,7 +14,7 @@ export default st => `
           <a id="material-links" href="https://www.espressoparts.com/brew/accessories/scales-timers" target="_blank"><li>Timer (or use your smart phone's timer)</li></a><br>
         </ul>
           <div class="steps">
-            <h3 id="steps">Step 1: Gather all materials & tools you will need</h3>
+            <h3 id="steps">Step 1: <br> Gather all materials & tools you will need</h3>
               <div id="step1imgs">
                 <img id="step1-img"  alt="coffee" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/sml-coffeebags.jpeg?raw=true">
                 <img id="step1-img"  alt="emptyscale" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/sml-empty-scale.jpeg?raw=true">
@@ -26,56 +26,56 @@ export default st => `
               <p id="setupdetail">You can reference the list at the top of the page</p>
           </div>
           <div class="steps">
-              <h3 id="steps" >Step 2: Select & weigh out your coffee</h3>
+              <h3 id="steps" >Step 2: <br> Select & weigh out your coffee</h3>
               <div class="step-img-div">
                     <img id="step-img" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/sml-weigh-scale.jpeg?raw=true">
                   </div>
-                  <p id="setupdetail">Start with fresh roasted whole bean coffee. <br> Weigh out 12g into each cup (or bowl) you intend to use <br><br> Using multiple cups will insure you are not only tasting <br>  a defect that may have made it into one of your cups</p>
+                  <p id="setupdetail">Start with fresh roasted whole bean coffee. <br> Weigh out 12g into each cup (or bowl) you intend to use <br><br> Using multiple cups will insure you are not only tasting a defect that may have made it into one of your cups</p>
             </div>
             <div class="steps">
-              <h3 id="steps">Step 3: Grind the coffee</h3>
+              <h3 id="steps">Step 3: <br> Grind the coffee</h3>
               <div class="step-img-div">
                     <img id="step-img" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/sml-grind-detail.jpeg?raw=true">
                   </div>
-                  <p id="setupdetail">You will want to 'purge' about 2-3 grams of each coffee through the grinder to <br>  eliminate the chance of anything left from any previously ground coffee   <br> <br> Repeat this step with each coffee you have  <br>  <br> * The ground coffee should resemble coarse sea salt *</p>
+                  <p id="setupdetail">You will want to 'purge' about 2-3 grams of each coffee through the grinder to eliminate the chance of anything left from any previously ground coffee <br> <br> Repeat this step with each coffee you have  <br><br> * The ground coffee should resemble coarse sea salt *</p>
               </div>
               <div class="steps">
-                <h3 id="steps">Step 4: Set up your cupping area</h3>
+                <h3 id="steps">Step 4: <br> Set up your cupping area</h3>
                     <div class="step-img-div">
                       <img id="step4-img" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/cuppingtable.jpeg?raw=true">
                     </div>
-                    <p id="setupdetail">Space out the each set of cups with rinsing cup of hot water <br><br> Use the <a href="/Numbers" id="num-link" target="_blank">numbers</a> tool in to generate placeholders for your coffee's name <br><br> This will help keep the cupping 'blind' and remove  preconceived <br> notions of what each coffee could/would/should taste like</p>
+                    <p id="setupdetail">Space out the each set of cups with rinsing cup of hot water <br><br> Use the <a href="/Numbers" id="num-link" target="_blank">numbers</a> tool in to generate placeholders for your coffee's name <br><br> This will help keep the cupping 'blind' and remove  preconceived notions of what each coffee could/would/should taste like</p>
               </div>
               <div class="steps">
-                <h3 id="steps">Step 5: Assess the color and aroma of the dry coffee</h3>
+                <h3 id="steps">Step 5: <br> Assess the color and aroma of the dry coffee</h3>
                     <div class="step-img-div">
                         <img id="step-img" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/sml-dry.jpeg?raw=true">
                     </div>
                     <p id="setupdetail">Take notes with the numbers as placeholders for the coffee's actually name</p>
               </div>
               <div class="steps">
-                <h3 id="steps">Step 6: Start timer and wet your coffee</h3>
+                <h3 id="steps">Step 6: <br> Start timer and wet your coffee</h3>
                     <div class="step-img-div">
                       <img id="step-img" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/sml-pouring.jpeg?raw=true">
                     </div>
                     <p id="setupdetail">You want to use water at a temperature just below boiling. (198°F-208°F)  <br> <br> Be sure to fully saturate the coffee in the bowl and fill near the top</p>
               </div>
               <div class="steps">
-                <h3 id="steps">Step 7: Assess the aroma of the wet coffee</h3>
+                <h3 id="steps">Step 7: <br> Assess the aroma of the wet coffee</h3>
                     <div class="step-img-div">
                       <img id="step-img" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/sml-wet.jpeg?raw=true">
                     </div>
                     <p id="setupdetail">Take note of any differences or new smells from the dry coffee.</p>
               </div>
               <div class="steps">
-                <h3 id="steps">Step 8: At the 4 minute mark, break the crust</h3>
+                <h3 id="steps">Step 8: <br> At the 4 minute mark, break the crust</h3>
                   <div class="step-img-div">
                       <img id="step-img" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/sml-break.jpeg?raw=true">
                     </div>
                     <p id="setupdetail">Get in close to the bowl, then using a spoon <br> push the coffee around slowly front to back <br> <br> * Be sure to rinse your spoon in hot water between each bowl * </p>
               </div>
               <div class="steps">
-                <h3 id="steps">Step 9: Using two spoons, remove any remaining <br> coffee grounds or foam floating at the top.</h3>
+                <h3 id="steps">Step 9: <br> Using two spoons, remove any remaining coffee grounds or foam floating at the top.</h3>
                     <div class="step-img-div">
                       <img id="step910-img" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/sml-skim1.jpeg?raw=true">
                       <img id="step910-img" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/sml-skim2.jpeg?raw=true">
@@ -83,7 +83,7 @@ export default st => `
                     <p id="setupdetail">You can use a extra mug to dispense of those</p>
               </div>
               <div class="steps">
-                <h3 id="steps">Step 10: Allow the coffee to cool until the 12-15 minute mark, then begin tasting. <br> Get a small amount of the coffee in your spoon, then SLURP!</h3>
+                <h3 id="steps">Step 10: <br> Allow the coffee to cool until the 12-15 minute mark, then begin tasting. <br> Get a small amount of the coffee in your spoon, then SLURP!</h3>
                   <p id="setupdetail"> * be sure to rinse your spoon in hot water between cups *</p>
                       <div class="step-img-div">
                       <img id="step910-img" src="https://github.com/RenwickRyan/Cupp---Savvy-Capstone/blob/master/Photos/sml-slurp1.jpeg?raw=true">
--- Components/Views/Resources.js
+++ Components/Views/Resources.js
@@ -3,24 +3,23 @@ export default () => `
     <div class="numbers">
       <button class="btn" onclick="location.reload()">click here</button>
         <h3 id="num-text">Below is some numbers you can use as placeholders for the names of the coffee you are use to keep your cupping 'blind'.</h3>
-        <div class="num-display">
-          <p class="num" id="num-1"></p>
-          <p class="num" id="num-2"></p>
-          <p class="num" id="num-3"></p>
-          <p class="num" id="num-4"></p>
-          <p class="num" id="num-5"></p>
-          <p class="num" id="num-6"></p>
-          <p class="num" id="num-7"></p>
-          <p class="num" id="num-8"></p>
-          <p class="num" id="num-9"></p>
-          <p class="num" id="num-10"></p>
-        </div>
+    </div>
+    <div class="num-display">
+      <p class="num" id="num-1"></p>
+      <p class="num" id="num-2"></p>
+      <p class="num" id="num-3"></p>
+      <p class="num" id="num-4"></p>
+      <p class="num" id="num-5"></p>
+      <p class="num" id="num-6"></p>
+      <p class="num" id="num-7"></p>
+      <p class="num" id="num-8"></p>        <p class="num" id="num-9"></p>
+      <p class="num" id="num-10"></p>
     </div>
   <div class="tasting-wheel">
-    <img id="tasting-wheel" src="https://coffee-bike.com/wp-content/uploads/2018/07/flavor-wheel-en-983x800.png" alt="coffee tasting wheel" style="width:1000px;height:auto;">
+    <img id="tasting-wheel" src="https://coffee-bike.com/wp-content/uploads/2018/07/flavor-wheel-en-983x800.png" alt="coffee tasting wheel">
   </div>
   <div class="cupping-form">
-    <img id="cupping-form" src="https://assets.echocommunity.org/presentations/a87a1fed-d2bc-4c55-b50e-12cc5d975e05/scaa-cupping-form__0.jpg" alt="coffee cupping form" style="width:1000px;height:auto;">
+    <img id="cupping-form" src="https://assets.echocommunity.org/presentations/a87a1fed-d2bc-4c55-b50e-12cc5d975e05/scaa-cupping-form__0.jpg" alt="coffee cupping form">
   </div>
 </main>
 `;
